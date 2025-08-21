1ef88578456fa0727832494879c36c1688f7681b
AngelCaesarMFarraronsJr <farraronsangelcaesar@gmail.com>
2025-08-21 20:21:42 +0800

    Array.cpp

--- Array.cpp
+++ Array.cpp
@@ -2,17 +2,34 @@
 #include <stdexcept>
 #include "Array.h"
 
-// Constructor: allocate array with given capacity
 Array::Array(int capacity) : capacity_(capacity), size_(0) {
     data_ = new int[capacity_];
 }
 
-// Destructor: free allocated memory
 Array::~Array() {
     delete[] data_;
 }
 
-// Add value at the end if there's space
+bool Array::IsValidIndex(int index) const {
+    return index >= 0 && index < size_;
+}
+
+bool Array::IsValidInsertIndex(int index) const {
+    return index >= 0 && index <= size_;
+}
+
+void Array::ShiftRight(int startIndex) {
+    for (int i = size_; i > startIndex; i--) {
+        data_[i] = data_[i - 1];
+    }
+}
+
+void Array::ShiftLeft(int startIndex) {
+    for (int i = startIndex; i < size_ - 1; i++) {
+        data_[i] = data_[i + 1];
+    }
+}
+
 bool Array::Add(int value) {
     if (size_ == capacity_) {
         return false;  // no space
@@ -22,22 +39,52 @@ bool Array::Add(int value) {
     return true;
 }
 
-// Insert value at specified index, shifting elements to the right
 bool Array::InsertAt(int index, int value) {
-    return false;
+   
+    if (size_ == capacity_ || !IsValidInsertIndex(index)) {
+        return false;
+    }
+    
+    
+    ShiftRight(index);
+    
+    
+    data_[index] = value;
+    size_++;
+    
+    return true;
 }
 
-// Delete element at index, shifting elements left
+
 bool Array::DeleteAt(int index) {
-    return false;
+    
+    if (!IsValidIndex(index)) {
+        return false;
+    }
+    
+    
+    ShiftLeft(index);
+    
+    
+    size_--;
+    
+    return true;
 }
 
-// Update element at index
+
 bool Array::UpdateAt(int index, int new_value) {
-    return false;
+    
+    if (!IsValidIndex(index)) {
+        return false;
+    }
+    
+    
+    data_[index] = new_value;
+    
+    return true;
 }
 
-// Print all elements
+
 void Array::Print() const {
     if (size_ == 0) {
         std::cout << "[Empty]" << std::endl;
@@ -51,17 +98,17 @@ void Array::Print() const {
     std::cout << " ]" << std::endl;
 }
 
-// Get current size
+
 int Array::Size() const {
     return size_;
 }
 
-// Get max capacity
+
 int Array::Capacity() const {
     return capacity_;
 }
 
-// Get value at index or throw if invalid
+
 int Array::GetAt(int index) const {
     if (index < 0 || index >= size_) {
         throw std::out_of_range("Index out of range");
